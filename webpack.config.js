159a0269535398cd15d2e35848771a421ba30890
hellomac87 <hellomac87@gmail.com>
2020-07-07 10:21:17 +0900

    바벨 사용법과 웹팩 통합

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./app.js",
   },
   output: {
     path: path.resolve("./dist"),
@@ -33,6 +33,11 @@ module.exports = {
           name: "[name].[ext]?[hash]",
           limit: 20000, //20kb url-loader 가 file 을 처리할때 2kb 미만의 파일은 data url 로 변환 그 이상은 file-loader 가 처리
         },
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
     ],
   },
