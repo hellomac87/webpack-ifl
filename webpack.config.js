205ecefa2a2a15a9798e01344639bfd721919163
hellomac87 <hellomac87@gmail.com>
2020-07-16 10:14:43 +0900

    최적화 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,135 +1,108 @@
 const path = require("path");
 const webpack = require("webpack");
-const childProcess = require("child_process");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const apiMocker = require("connect-api-mocker");
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-
-const mode = process.env.NODE_ENV || 'development';
+const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
   mode,
   entry: {
-    main: "./src/app.js",
-    // result: "./src/result.js"
+    main: "./src/app.js"
   },
   output: {
-    path: path.resolve("./dist"),
     filename: "[name].js",
+    path: path.resolve("./dist")
   },
   devServer: {
-    // contentBase: path.join(__dirname, "dist"), 
-    // publicPath: "/", 
-    // host: "dev.domain.com",
-    overlay: true, // 빌드시 에러나 경고 문구를 브라우저 화면에 표시
-    port: 8080, // 개발서버 port 설정, 기본값은 8080
+    overlay: true,
     stats: "errors-only",
-    // historyApiFallback: true, // 히스토리 API를 사용하는 SPA 개발시 설정한다. 404가 발생하면 index.html로 리다이렉트한다.
-    before: (app) => {
-      // app : server 객체 (개발서버, express 객체)
-      // before 함수 내에서 webpack 개발서버의 기능을 추가 할 수 있음
-      // app.get('/api/users', (req, res) => {
-      //   res.json([
-      //     {
-      //       id: 1,
-      //       name: "Alice"
-      //     }, {
-      //       id: 2,
-      //       name: "asdf"
-      //     },
-      //     {
-      //       id: 3,
-      //       name: "qwer"
-      //     }
-      //   ])
-      // })
-      app.use(apiMocker('/api', 'mocks/api'));;
+    // before: (app, server, compiler) => {
+    //   app.use(apiMocker('/api', 'mocks/api'));
+    // },
+    proxy: {
+      "/api": "http://localhost:8081"
     },
-
     hot: true
-  },
-  optimization: {
-    minimizer: mode === 'production' ? [
-      new OptimizeCSSAssetsPlugin(),
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true, // 콘솔 로그를 제거한다 
-          }
-        }
-      })
-    ] : [],
-    // splitChunks: {
-    //   chunks: 'all'
-    // }
-  },
-  externals: {
-    axios: "axios"
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(scss|css)$/,
         use: [
-          process.env.NODE_ENV === "production"
-            ? MiniCssExtractPlugin.loader
-            : "style-loader",
+          mode === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
           "css-loader",
-        ],
+          "sass-loader"
+        ]
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
-          // publicPath: "./dist/",
           name: "[name].[ext]?[hash]",
-          limit: 20000, //20kb url-loader 가 file 을 처리할때 2kb 미만의 파일은 data url 로 변환 그 이상은 file-loader 가 처리
-        },
+          limit: 10000 // 10Kb
+        }
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
         exclude: /node_modules/,
-      },
-    ],
+        loader: "babel-loader" // 바벨 로더를 추가한다
+      }
+    ]
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `
-        Build Date: ${new Date().toLocaleString()}
-        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
-        Author: ${childProcess.execSync("git config user.name")}
-      `,
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
     }),
-    new webpack.DefinePlugin({
-      TWO: JSON.stringify("1+1"),
-      "api.domain": JSON.stringify("http://dev.api.domain.com"),
-    }),
+    new webpack.DefinePlugin({}),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+        env: mode === "development" ? "(개발용)" : ""
       },
       minify:
-        process.env.NODE_ENV === "production"
+        mode === "production"
           ? {
-            collapseWhitespace: true,
-            removeComments: true,
+            collapseWhitespace: true, // 빈칸 제거
+            removeComments: true // 주석 제거
           }
           : false,
+      hash: mode === "production"
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
-      ? [new MiniCssExtractPlugin({ filename: "[name].css" })]
-      : []),
-    new CopyPlugin([{
-      from: './node_modules/axios/dist/axios.min.js',
-      to: './axios.min.js' // 목적지 파일에 들어간다
-    }])
+    new CopyPlugin([
+      {
+        from: "./node_modules/axios/dist/axios.min.js",
+        to: "./axios.min.js" // 목적지 파일에 들어간다
+      }
+    ]),
+    ...(mode === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
   ],
+  optimization: {
+    minimizer:
+      mode === "production"
+        ? [
+          new OptimizeCSSAssetsPlugin(),
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: true // 콘솔 로그를 제거한다
+              }
+            }
+          })
+        ]
+        : []
+  },
+  externals: {
+    axios: "axios"
+  }
 };
