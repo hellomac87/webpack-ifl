b2f3de9e253f122195ba40959ae65d8f781aca89
hellomac87 <hellomac87@gmail.com>
2020-07-30 13:03:48 +0900

    자주 사용하는 플러그인

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
-const MyWebpackPlugin = require('./my-webpack-plugin');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -17,7 +21,9 @@ module.exports = {
             {
                 test: /\.css$/, // parttern
                 use: [
-                    'style-loader',
+                    process.env.NODE_ENV === 'production' ?
+                        MiniCssExtractPlugin.loader :
+                        'style-loader',
                     'css-loader'
                 ]
             },
@@ -25,7 +31,7 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'url-loader',
                 options: {
-                    publicPath: './dist/',
+                    // publicPath: './dist/',
                     name: '[name].[ext]?[hash]',
                     limit: 20000, //20kb
                 },
@@ -33,6 +39,30 @@ module.exports = {
         ]
     },
     plugins: [ // 번들된 결과물에 후처리
-        new MyWebpackPlugin(),
+        new webpack.BannerPlugin({
+            banner: `
+                Bulid Date: ${new Date().toLocaleString()}
+                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+                Auhtor: ${childProcess.execSync('git config user.name')}
+
+            `
+        }),
+        new webpack.DefinePlugin({
+            TWO: JSON.stringify('1+1'),
+            'api.domain': JSON.stringify('http://dev.api.domain.com')
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === "development" ? '(개발용)' : ''
+            },
+            minify: process.env.NODE_ENV === 'production' ? {
+                collapseWhitespace: true,
+                removeComments: true
+            } : false
+        }),
+        new CleanWebpackPlugin(),
+        ...(process.env.NODE_ENV === 'production' ?
+            [new MiniCssExtractPlugin({ filename: '[name].css' })] : [])
     ]
 }
